616aa35fcf649fb47b550186b47e86b630238679
Jeff Russell <jeffrussell1@yahoo.com>
2026-05-12 09:33:28 -0500

    bug fix

--- src/Tabs/SettingsTab.jsx
+++ src/Tabs/SettingsTab.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useRef } from 'react';
 import { useApp } from '../AppContext';
 import { dbFetch } from '../utils/storage';
 
