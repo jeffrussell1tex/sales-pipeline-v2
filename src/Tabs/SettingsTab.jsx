b50bb66b771d93d796c2b3c9ba576c111185393d
Jeff Russell <jeffrussell1@yahoo.com>
2026-05-29 10:04:17 -0500

    dupe 9

--- src/Tabs/SettingsTab.jsx
+++ src/Tabs/SettingsTab.jsx
@@ -18084,6 +18084,7 @@ const AdminView = ({ settings, setSettings, currentUser, setActiveTab, setAccoun
         'pain-points':          'pain-points',
         'customer-types':       'customer-types',
         'industries':           'industries',
+        'duplicates':           'duplicates',
         'competitors':          'competitors',
         'reasons-won':          'reasons-won',
         'reasons-lost':         'reasons-lost',
